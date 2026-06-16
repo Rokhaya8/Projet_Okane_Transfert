e46ada808e58d1a5849574297a273368e23e3c34
Zineb <chaouchzineb26@gmail.com>
2026-06-16 04:16:26 +0200

    chatbot + commencement de login et auth

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+
+  credentials = {
+    email: '',
+    password: '',
+    role: 'CLIENT' // Rôle par défaut
+  };
+
+  login() {
+    // Logique d'appel au service d'authentification
+    console.log('Tentative de connexion:', this.credentials);
+  }
+}
